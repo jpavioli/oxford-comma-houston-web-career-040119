7d95a5fefeb84d192bd14edd20846ec978ed20f3
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 19:42:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,12 @@
 def oxford_comma(array)
+  text_array = []
   if array.length == 1 
-    array
+    text_array << array
   else 
-    
+    for i in (0..array.length-2) do
+      text_array << "#{array[i]}, "
+    end
+    text_array << "and #{array[-1]}"
+  end
+  text_arry.join
 end
