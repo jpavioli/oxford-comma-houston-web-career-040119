bfcfe7e14392c275a1d82e3a5a56c922f7bb1b3a
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-23 19:47:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   text_array = []
   if array.length == 1 
     text_array << array
+  elsif array.length == 2 
+    text_array << "#{array[0]} and #{array[1]}"
+  
   else 
     for i in (0..array.length-2) do
       text_array << "#{array[i]}, "
